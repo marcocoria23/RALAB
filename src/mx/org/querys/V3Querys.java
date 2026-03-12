d817c66960c2167015759fa96542f8af874c69fa
ANTONIO.CORIA <ANTONIO.CORIA@LC094958ASEGSPJ.inegi.gob.mx>
2026-03-12 11:40:44 -0600

    actualizacion de Query clave_organo

--- src/mx/org/querys/V3Querys.java
+++ src/mx/org/querys/V3Querys.java
@@ -70,9 +70,9 @@ public ArrayList PeriodoNEstatus(){
      public ArrayList Clave_organoNE(){
      conexion.Conectar();
       Array = new ArrayList();
-      sql="select unique(CLAVE_ORGANO)asCLAVE_ORGANO from V3_TR_CONTROL_EXPEDIENTEJL\n" +
+      sql="select unique(CLAVE_ORGANO)as CLAVE_ORGANO from V3_TR_CONTROL_EXPEDIENTEJL\n" +
            "UNION\n" +
-           "select unique(CLAVE_ORGANO)CLAVE_ORGANO from V3_TMP_CONTROL_EXPEDIENTEJL ORDER BY 1";
+           "select unique(CLAVE_ORGANO) as CLAVE_ORGANO from V3_TMP_CONTROL_EXPEDIENTEJL ORDER BY 1";
       System.out.println(sql);
       resul=conexion.consultar(sql);
       try {
