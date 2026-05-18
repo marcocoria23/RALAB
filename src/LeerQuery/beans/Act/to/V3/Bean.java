b5ce5e43a3698715c830b91a2408ac161a4b346b
ANTONIO.CORIA <ANTONIO.CORIA@LC094958ASEGSPJ.inegi.gob.mx>
2026-05-18 10:11:15 -0600

    Se agrega clase Bean

---
+++ src/LeerQuery/beans/Act/to/V3/Bean.java
@@ -0,0 +1,22 @@
+package LeerQuery.beans.Act.to.V3;
+
+;
+import Bean_Procedures.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public abstract class Bean
+{
+    
+   
+   
+    public abstract Object[] getLlave();
+  
+    public abstract Object[] toArray();
+    
+    public abstract void setAtributos(Object[] obj);       
+    
+    public abstract void actualizaDesc(Connection conn) throws SQLException ;
+  
+}
